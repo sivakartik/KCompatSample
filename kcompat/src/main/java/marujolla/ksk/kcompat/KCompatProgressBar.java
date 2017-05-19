bcc07084500975699577ca2913199de31421b898
shiva kartik marujolla <smarujo@searshc.com>
2017-05-19 09:12:29 -0500

    added setIndeterminateTintList method so we can change color in runtime

--- kcompat/src/main/java/marujolla/ksk/kcompat/KCompatProgressBar.java
+++ kcompat/src/main/java/marujolla/ksk/kcompat/KCompatProgressBar.java
@@ -7,6 +7,7 @@ import android.content.res.TypedArray;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
 
@@ -46,21 +47,48 @@ public class KCompatProgressBar extends ProgressBar {
             mProgressTintList = typedArray.getColorStateList(R.styleable.KCompatProgressBar_kProgressTint);
         }
         typedArray.recycle();
-        applyProgressTint();
+        applyIndeterminateProgressTint();
     }
 
-    public void applyProgressTint() {
+    public void applyIndeterminateProgressTint() {
         if (mProgressTintList != null) {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
                 //The indeterminateTint to change progress bar color doesn't work below API level 21.
-               // we are creating a new drawable so that it doesn't effect the other progressBar
                 Drawable drawable = getIndeterminateDrawable().getConstantState().newDrawable().mutate();
-                drawable.setColorFilter(mProgressTintList.getDefaultColor(),
-                        PorterDuff.Mode.SRC_IN);
-                setIndeterminateDrawable(drawable);
+                if (drawable != null) {
+                    if(mProgressTintList == null){
+                        // we are clearing the color filter if its null
+                        drawable.clearColorFilter();
+                    } else {
+                        // we are creating a new drawable so that it doesn't effect the other progressBar
+                        drawable.setColorFilter(mProgressTintList.getDefaultColor(),
+                                PorterDuff.Mode.SRC_IN);
+                    }
+                    setIndeterminateDrawable(drawable);
+                }
             } else {
-                setIndeterminateTintList(mProgressTintList);
+                super.setIndeterminateTintList(mProgressTintList);
             }
         }
+    }
+
+    /**
+     * Applies a tint to the indeterminate drawable. Does not modify the
+     * current tint mode, which is {@link PorterDuff.Mode#SRC_IN} by default.
+     * <p>
+     * Subsequent calls to {@link #setIndeterminateDrawable(Drawable)} will
+     * automatically mutate the drawable and apply the specified tint and
+     * tint mode using
+     * {@link Drawable#setTintList(ColorStateList)}.
+     *
+     * @param tint the tint to apply, may be {@code null} to clear tint
+     *
+     * @attr ref android.R.styleable#ProgressBar_indeterminateTint
+     * @see #getIndeterminateTintList()
+     * @see Drawable#setTintList(ColorStateList)
+     */
+    public void setIndeterminateTintList(@Nullable ColorStateList tint) {
+        mProgressTintList = tint;
+        applyIndeterminateProgressTint();
     }
 }
